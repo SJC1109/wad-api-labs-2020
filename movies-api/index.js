784ddccfd318f20007d2469d73aabe8c4a6d61ec
Jiacheng Shen <1449924798@qq.com>
2020-12-27 19:18:31 +0000

    Week 9 Error HAndling

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,7 +2,14 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
-
+const errHandler = (err, req, res, next) => {
+    /* if the error in development then send stack trace to display whole error,
+    if it's in production then just send error message  */
+    if(process.env.NODE_ENV === 'production') {
+      return res.status(500).send(`Something went wrong!`);
+    }
+    res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+  };
 dotenv.config();
 
 const app = express();
@@ -13,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
-
+app.use(errHandler);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
