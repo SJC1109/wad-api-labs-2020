63ed6e2a2965a08d5a55bbfe8e26d13a88ae266b
Jiacheng Shen <1449924798@qq.com>
2020-12-27 21:23:17 +0000

    Week 9 Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
+import usersRouter from './api/users';
 const errHandler = (err, req, res, next) => {
     /* if the error in development then send stack trace to display whole error,
     if it's in production then just send error message  */
@@ -24,6 +25,7 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
+app.use('/api/users', usersRouter);
 app.use('/api/movies', moviesRouter);
 app.use(errHandler);
 app.listen(port, () => {
